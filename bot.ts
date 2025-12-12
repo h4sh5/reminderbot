5eb7cab858923eace30e2e4f3a410ed7f1531bd9
h4sh <38898566+h4sh5@users.noreply.github.com>
2025-12-12 16:26:59 +1000

    Create bot.ts

---
+++ bot.ts
@@ -0,0 +1,24 @@
+
+const postData = async () => {
+ 
+  try {
+    const response = await fetch(process.env.WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"content":"@everyone what did you work on today? Send a link of your contributions."});
+    });
+ 
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+ 
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+ 
+postData();
